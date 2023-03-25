45964e5b22d2b36bb70b6fcbc9dbd18166f9e688
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-03-25 17:04:14 +0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App';
-import { UserStore } from './src/user/UserContext';
+import App from './App';
+import { UserStore } from './user/UserContext';
 
 export const Context = createContext(null);
 
