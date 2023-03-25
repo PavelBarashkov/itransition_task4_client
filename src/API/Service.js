366e0175215cadf19e6a39c3304ce85fa321a4d5
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-03-25 13:27:32 +0400

    Updated by URL

--- src/API/Service.js
+++ src/API/Service.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 export default class PostService {
     static async getAll() {
-        const response =  await axios.get('http://localhost:5000/api/user/users');
+        const response =  await axios.get('https://task4-server-o7ux.onrender.com/api/user/users');
         return response;
     }
     static  async deleteUserId(id) {
-        const response =  await axios.delete(`http://localhost:5000/api/user/users/${id}`);
+        const response =  await axios.delete(`https://task4-server-o7ux.onrender.com/api/user/users/${id}`);
         return response;
     }
     static async dataUpdateId(id, status) {
-        const response = await axios.put(`http://localhost:5000/api/user/users/${id}`, {status});
+        const response = await axios.put(`https://task4-server-o7ux.onrender.com/api/user/users/${id}`, {status});
         return response;
     } 
 }
